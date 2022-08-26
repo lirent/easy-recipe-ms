8f1c31ff3008456dc50a5bb2014d68f28f24d3d2
Lirent P <live@lirent.net>
2022-08-26 22:45:29 +0200

    Initial commit

---
+++ src/main/java/net/lirent/recipemanager/Application.java
@@ -0,0 +1,18 @@
+package net.lirent.recipemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot application
+ * @author Lirent
+ */
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
